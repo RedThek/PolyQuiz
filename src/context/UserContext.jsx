694a69b4c019748556c178e0294801a74352d587
MOLA <91349290+RedThek@users.noreply.github.com>
2026-05-28 14:59:03 +0100

    Jalon 3 : Sécurisation via le Routage Avancé - Configuration de trois routes principales avec react-router-dom - Creation du composant <ProtectedRoute>

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,9 @@
 import React, { createContext, useState } from 'react';
 
-// 1. Instanciation du contexte global [cite: 25]
+// 1. Instanciation du contexte global pour stocker les données utilisateur (pseudo et meilleur score)
 export const UserContext = createContext();
 
-// 2. Création du composant Provider [cite: 25]
+// 2. Création du composant Provider qui enveloppe l'application et fournit les données globales
 export const UserProvider = ({ children }) => {
   // L'état global contient le pseudo (null par défaut) et le meilleur score [cite: 26]
   const [pseudo, setPseudo] = useState(null);
